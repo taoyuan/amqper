07d14061376b1c5b52c969dd01b32e2657507c78
taoyuan <torworx@gmail.com>
2015-07-29 17:16:06 +0800

    client emit error when handler has an error

--- test/amqper.test.js
+++ test/amqper.test.js
@@ -11,7 +11,7 @@ function delayCloseClient(client, done) {
 }
 
 describe('amqper', function () {
-  this.timeout(8000);
+  this.timeout(10000);
 
   describe('connect', function () {
     it('should connect to rabbit server', function (done) {
@@ -32,7 +32,6 @@ describe('amqper', function () {
       var client = amqper.connect('amqp://guest:guest@localhost:5672');
       client.$promise.then(function () {
         client.route('test1.:arg', {queue: 'this_is_queue_name_1'}, function (message) {
-          console.log(message.payload);
           t.deepEqual(message.payload, data);
           delayCloseClient(client, done);
         }).then(function () {
@@ -52,7 +51,6 @@ describe('amqper', function () {
       client.$promise.then(function () {
         client.format('msgpack');
         client.route('test2.:arg', {queue: 'this_is_queue_name_2'}, function (message) {
-          console.log(message.payload);
           t.deepEqual(message.payload, data);
           delayCloseClient(client, done);
         }).then(function () {
@@ -60,5 +58,25 @@ describe('amqper', function () {
         });
       });
     });
+
+    it('should handle the error in handler', function (done) {
+      var data = {
+        hello: 'world'
+      };
+
+      var client = amqper.connect('amqp://guest:guest@localhost:5672');
+      client.$promise.then(function () {
+        client.route('test2.:arg', {queue: 'this_is_queue_name_2'}, function () {
+          throw new Error('boom');
+        }).then(function () {
+          client.publish('amq.topic', 'test2.a', data);
+        });
+      });
+      client.on('error', function (err) {
+        t.equal(err.message, 'boom');
+        delayCloseClient(client, done);
+      });
+    });
   });
+
 });
