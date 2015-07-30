67859c2e44b63b4346fad05179610c9632766956
taoyuan <torworx@gmail.com>
2015-07-30 15:54:01 +0800

    make pub ans sub safe for call before connected

--- examples/round-robin/consumer.js
+++ examples/round-robin/consumer.js
@@ -6,7 +6,7 @@ var client = amqper.connect('amqp://guest:guest@localhost:5672');
 
 client.$promise.then(function () {
   console.log('ready');
-  client.route('test.a', function (message) {
+  client.route('test.a', {}, function (message) {
     console.log(message.payload);
   });
 });
